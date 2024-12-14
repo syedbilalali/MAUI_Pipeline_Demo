855ab70eab1116789697714f440347ccc9c2c322
syedbilalali <syedbilalali.dev@gmail.com>
2024-12-14 15:04:31 +0530

    Project Setup Code and Github Action Work Flow Added

---
+++ src/MAUI_PiplineDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_PiplineDemo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/MAUI_PiplineDemo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_PiplineDemo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
